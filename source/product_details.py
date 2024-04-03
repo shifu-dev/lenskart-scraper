1446e186d869695c22770657132fd6bbe74bdc86
jenisha <jenisha.cs20@bitsathy.ac.in>
2024-04-03 13:00:20 +0530

    Scrapper for eyeglasses

--- source/product_details.py
+++ source/product_details.py
@@ -1,15 +1,21 @@
 class Details:
     product_title = ""
-    product_price = 0
-    product_currency = ""
+    product_collection = ""
     product_size = ""
+    product_price = 0
+    discounted_price = ""
+    coupen_code = ""
+    product_rating = ""
 
     def __str__(self) -> str:
         result = (
             f"product_title: {self.product_title}\n"
-            f"product_price: {self.product_price}\n"
-            f"product_currency: {self.product_currency}\n"
+            f"product_collection: {self.product_collection}\n"
             f"product_size: {self.product_size}\n"
+            f"product_price: {self.product_price}\n"
+            f"discounted_price: {self.discounted_price}\n"
+            f"coupen_code: {self.coupen_code}\n"
+            f"product_rating: {self.product_rating}\n"
         )
 
         return result
