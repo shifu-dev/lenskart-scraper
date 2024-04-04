1f99e386b32b08e8e62aa01c3b6a66643844dfce
shifu-dev <csingh8476@gmail.com>
2024-04-04 19:27:20 +0530

    refactor: refactor stores.

---
+++ source/stores.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class scraper:
+    def scrape_store(url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        details = {}
+
+        details["Store Name"] = soup.find("h1", class_="Home_name__J6U_a").text.strip()
+        details["Address"] = soup.find("div", class_="Home_wrapper__ARCSA").text.strip()
+        timing_text = soup.find("div", class_="Home_infoBox__PV5Wz").text.strip()
+        details["Timing"] = timing_text.split(".Close")[1]
+        details["Service"] = (
+            soup.find_all("span", class_="Home_miniHead__KKq3S")[1]
+            .find_next_sibling()
+            .text.strip()
+        )
+        tel_links = soup.find_all("a", href=lambda href: href and "tel:" in href)
+        details["Phone Number"] = tel_links[1]["href"].split(":")[1]
+        details["Google Map Link"] = soup.find(
+            "a", href=lambda href: href and "maps.google.com" in href
+        )["href"]
+        review_count = soup.find("div", class_="Home_count__Y0nOJ").text.strip()
+        rating = (
+            soup.find("div", class_="Home_rating__BaBug").text.strip() + review_count
+        )
+        details["Rating"] = rating
+
+        nearby_stores = soup.find_all("div", class_="StoreCard_halfCard__X8eye")
+        nearby_store_details = []
+        for store in nearby_stores:
+            nearby_store_name = (
+                store.find("div", class_="StoreCard_name__mrTXJ")
+                .find("span")
+                .text.strip()
+            )
+            nearby_store_address = (
+                store.find("div", class_="storeDetials").find("span").text.strip()
+            )
+            nearby_store_distance = nearby_store_address.split(".")[1:][0]
+            nearby_store_details.append(
+                {"Store Name": nearby_store_name, "Distance": nearby_store_distance}
+            )
+
+        details["Nearby Stores"] = nearby_store_details
+
+        return details
