a42f53d3a51c4c11d61c4e6f5f3a3ac7f21d7237
shifu-dev <csingh8476@gmail.com>
2024-04-05 10:48:28 +0530

    update: improve stores module.

--- source/stores.py
+++ source/stores.py
@@ -2,32 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class scraper:
-    def scrape_store(url):
+class StoreDetails:
+    name: str
+    address: str
+    timing: str
+    service: str
+    contact_number: int
+    gmap_link: str
+    rating: int
+    nearby_stores: object
+
+
+class StoreScraper:
+    def scrap(url):
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
 
-        details = {}
+        details = StoreDetails()
 
-        details["Store Name"] = soup.find("h1", class_="Home_name__J6U_a").text.strip()
-        details["Address"] = soup.find("div", class_="Home_wrapper__ARCSA").text.strip()
+        details.name = soup.find("h1", class_="Home_name__J6U_a").text.strip()
+        details.address = soup.find("div", class_="Home_wrapper__ARCSA").text.strip()
         timing_text = soup.find("div", class_="Home_infoBox__PV5Wz").text.strip()
-        details["Timing"] = timing_text.split(".Close")[1]
-        details["Service"] = (
+        details.timing = timing_text.split(".Close")[1]
+        details.service = (
             soup.find_all("span", class_="Home_miniHead__KKq3S")[1]
             .find_next_sibling()
             .text.strip()
         )
         tel_links = soup.find_all("a", href=lambda href: href and "tel:" in href)
-        details["Phone Number"] = tel_links[1]["href"].split(":")[1]
-        details["Google Map Link"] = soup.find(
+        details.contact_number = tel_links[1]["href"].split(":")[1]
+        details.gmap_link = soup.find(
             "a", href=lambda href: href and "maps.google.com" in href
         )["href"]
         review_count = soup.find("div", class_="Home_count__Y0nOJ").text.strip()
         rating = (
             soup.find("div", class_="Home_rating__BaBug").text.strip() + review_count
         )
-        details["Rating"] = rating
+        details.rating = rating
 
         nearby_stores = soup.find_all("div", class_="StoreCard_halfCard__X8eye")
         nearby_store_details = []
@@ -45,6 +56,38 @@ class scraper:
                 {"Store Name": nearby_store_name, "Distance": nearby_store_distance}
             )
 
-        details["Nearby Stores"] = nearby_store_details
+        details.nearby_stores = nearby_store_details
 
         return details
+
+
+class StoreExporter:
+    def __init__(self, writer: object) -> None:
+        writer.writerow(
+            [
+                "Name",
+                "Address",
+                "Timing",
+                "Service",
+                "Contact Number",
+                "Google Map Link",
+                "Rating",
+                "Nearby Stores",
+            ]
+        )
+
+    def add(self, details) -> None:
+        self.writer.writerow(
+            [
+                details.name,
+                details.address,
+                details.timing,
+                details.service,
+                details.contact_number,
+                details.gmap_link,
+                details.rating,
+                details.nearby_stores,
+            ]
+        )
+
+    writer: object
